ccab2b5487c3c5c1f8e8b1ca2e560318a7d5049c
juliolemus <julio165guyl@gmail.com>
2015-02-27 15:36:12 -0800

    mod 12

--- StorkTower.rb
+++ StorkTower.rb
@@ -7,13 +7,23 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = "kLpHRgFrKeCcEiZY6gn37mukVbwojabl3GTQTQYTjstIv"
 end
 
+hashTags = ['#HarryPotterThemeSong',
+      '#HungerGamesSoundtrack',
+      '#DisneyMusic',
+      '#ChistmasMusic']
 while true 
-	if Time.now.min == 0
-		post = "BONG "*Time.now.hour % 12
-		client.update(post[0..-1])
-		puts "Success"
-		sleep()
-	end
-
+  t = Time.now
+  if t.min == 0
+    current_hour = t.hour % 12
+    post = "BONG " * current_hour
+    client.update(post[0..-1])
+    puts "Posted BONG to Twitter #{current_hour} times at #{t.strftime('%r')}" 
+    sleep(70)
+  elsif t.sunday? and t.hour == 11 and t.min == 30
+    current_song = hashTags[rand(0...songs.length)]
+    client.update(current_song)
+    puts "Posted #{current_song} to Twitter at #{t.strftime('%r')}" 
+    sleep(70)
+  end
 end 
 
