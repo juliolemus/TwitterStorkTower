18ed6f7e462baff139469d00a5dc60bd0844684e
juliolemus <julio165guyl@gmail.com>
2015-02-27 16:00:09 -0800

    gitignore api keys

---
+++ secrets.rb
@@ -0,0 +1,8 @@
+require 'twitter'
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "BvrlDf78bXjEBjNcB7Bj4bQg7"
+  config.consumer_secret     = "iyxyHXk628rRf44dc3O8Q7K0psipgsKX3RcBkRzLhlIh3sTcTx"
+  config.access_token        = "3047831041-nRAFKXq3WrhNxhRiAfpxgg82aMogMlx2UzmgTPO"
+  config.access_token_secret = "kLpHRgFrKeCcEiZY6gn37mukVbwojabl3GTQTQYTjstIv"
+end
