67a8fb0e0e5d7ac0140341f5a14f8db1fc5e33d7
juliolemus <julio165guyl@gmail.com>
2015-02-27 20:54:38 -0800

    Latest Changes

--- secrets.rb
+++ secrets.rb
@@ -1,7 +1,7 @@
 require 'twitter'
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "8qXpVYBwtsx7no3GRUAY3FOrR"
+  config.consumer_key        = "XpVYBwtsx7no3GRUAY3FOrR"
   config.consumer_secret     = "5tqlliO6tZZe98PD9MtLRhwngq2K1DGa53SIi5yhzzYfCRVgDx"
   config.access_token        = "3047831041-nRAFKXq3WrhNxhRiAfpxgg82aMogMlx2UzmgTPO"
   config.access_token_secret = "kLpHRgFrKeCcEiZY6gn37mukVbwojabl3GTQTQYTjstIv"
