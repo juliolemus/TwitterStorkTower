35a2cf4f3ed069914ead64f278fa576b250b6fea
juliolemus <julio165guyl@gmail.com>
2015-02-27 20:49:39 -0800

    Changed Permissions again

--- secrets.rb
+++ secrets.rb
@@ -1,8 +1,8 @@
 require 'twitter'
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "UAezPbfScqJWgjdXcCvIluAVB"
-  config.consumer_secret     = "s1ksLXcBzZ03frb1YoSzPx737Bhb3Q2o8x8nRyoS9ilnpQG0ad"
+  config.consumer_key        = "8qXpVYBwtsx7no3GRUAY3FOrR"
+  config.consumer_secret     = "5tqlliO6tZZe98PD9MtLRhwngq2K1DGa53SIi5yhzzYfCRVgDx"
   config.access_token        = "3047831041-nRAFKXq3WrhNxhRiAfpxgg82aMogMlx2UzmgTPO"
   config.access_token_secret = "kLpHRgFrKeCcEiZY6gn37mukVbwojabl3GTQTQYTjstIv"
 end
